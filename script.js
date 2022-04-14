0a737650be2ba6413731e6a3c6af3745876c58ba
Asa designer web <94604570+aliakbarasa@users.noreply.github.com>
2022-04-14 16:26:19 +0430

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+//Hello friends, I am asadesignerweb. To watch this educational video, visit our YouTube channel and watch the address.
+//https://www.youtube.com/channel/UChna7rX4al4Vk61BWd56mJQ
+//Or to get the source code to the address
+//https://github.com/aliakbarasa/html-css-js
+//Go thank you very much I love you so much
